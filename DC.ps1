4da34c3153d567dea8073372367ea4efc52190bb
Maytason <med.ben.98@gmail.com>
2023-04-15 02:51:39 -0400

    adding script for DC install

---
+++ DC.ps1
@@ -0,0 +1,18 @@
+﻿## Script Création Active Directory
+## v1.0
+## Crée par : Mehdi B.
+## CREATION FORET AD
+
+Get-WindowsFeature
+
+# Installer le rôle AD-DS
+Add-WindowsFeature  -Name AD-Domain-Services  -IncludeManagementTools
+
+# Configurer la création d'une FORET AD
+Install-ADDSForest  -CreateDnsDelegation:$false `
+    -DomainName "esgi.lab"  -DomainNetbiosName  "ESGI"  `
+    -InstallDns:$true  -ForestMode 7  -DomainMode 7  `
+    -DatabasePath "C:\Windows\NTDS"  -SysvolPath "C:\Windows\SYSVOL"  `
+    -LogPath  "C:\Windows\NTDS"    `
+    -NoRebootOnCompletion:$false -Force:$true  -WhatIf
+
